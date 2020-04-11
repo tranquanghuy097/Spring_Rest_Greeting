11f776b59b0f92ea5bbea71cfbd4cd32d7325268
huy <tranquanghuy097@gmail.com>
2020-04-11 20:45:38 +0700

    GetList, FindByID done

---
+++ src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @Autowired
+    private GreetingRepo repo;
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/greeting_list")
+    public List<Greeting> getList() {
+        return repo.getAllGreetings();
+    }
+
+    @GetMapping("/greeting_id")
+    public Greeting getByID(@RequestParam(value = "id") Long id){
+        return repo.findByID(id);
+    }
+
+
+}
---
+++ src/main/java/com/example/demo/GreetingRepo.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public class GreetingRepo {
+    private static Map<Long, Greeting> greetingMap = new HashMap<Long, Greeting>();
+
+    static {
+        initGreet();
+    }
+
+    private static void initGreet()
+    {
+        Greeting greet1 = new Greeting(1, "a");
+        Greeting greet2 = new Greeting(2, "b");
+        Greeting greet3 = new Greeting(3, "c");
+
+        greetingMap.put(greet1.getId(), greet1);
+        greetingMap.put(greet2.getId(), greet2);
+        greetingMap.put(greet3.getId(), greet3);
+    }
+
+    public Greeting findByID(Long id){
+        return greetingMap.get(id);
+    }
+
+    public List<Greeting> getAllGreetings() {
+        Collection<Greeting> c = greetingMap.values();
+        List<Greeting> list = new ArrayList<Greeting>();
+        list.addAll(c);
+        return list;
+    }
+}
